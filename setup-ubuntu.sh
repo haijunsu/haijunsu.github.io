00ebf1d17eb16178747407f8bff27b8fb4d2fea8
Haijun Su <navysu@gmail.com>
2022-03-22 17:05:17 -0400

    change to setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+sudo apt update
 sudo apt install -y build-essential ruby-full ruby-bundler
 if [ ! -f "Gemfile" ]; then
   ln -s Gemfile.linux Gemfile
 fi
-sudo bundle install
+# sudo bundle install
+bundle install
